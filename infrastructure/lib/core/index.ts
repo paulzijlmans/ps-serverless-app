247fb7ac12ce97f5e17e862f4b170b75f0c384e2
Paul Zijlmans <paul.zijlmans@devolksbank.nl>
2021-12-09 09:00:21 +0100

    added webapp to infrastructure

--- infrastructure/lib/core/index.ts
+++ infrastructure/lib/core/index.ts
@@ -1,10 +1,15 @@
 import * as cdk from '@aws-cdk/core';
 import { AssetStorage } from './storage';
+import { WebApp } from './webapp';
 
 export class ApplicationStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new AssetStorage(this, 'Storage');
+    const storage = new AssetStorage(this, 'Storage');
+
+    new WebApp(this, 'WebApp', {
+      hostingBucket: storage.hostingBucket,
+    });
   }
 }

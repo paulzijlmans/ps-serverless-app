f76a77b11d7741b3429771a888355edb84db66fc
Paul Zijlmans <paul.zijlmans@devolksbank.nl>
2021-12-27 15:45:38 +0100

    updated config

--- aws_bootstrap.sh
+++ aws_bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-. aws_keys
+. .config
 
 env CDK_NEW_BOOTSTRAP=1 npx cdk bootstrap \
 --cloudformation-execution-policies arn:aws:iam::aws:policy/AdministratorAccess \
-aws://$AWS_USER/us-east-1
+aws://$AWS_USER/$AWS_REGION
--- setup.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-./aws_configure.sh
-./aws_bootstrap.sh
